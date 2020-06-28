3131e1f9b853fcfc0b19cfa305d8f546ac4563ae
ellfiscina <ellfiscina@gmail.com>
2020-06-28 11:20:17 +0200

    add typing animation

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Typing from 'react-typing-animation';
 import "./Banner.css";
 
 class Banner extends Component {
     render() {
       return (
         <div className="Banner" id="home">
-            <h1 className="Banner-title">Hi, I am Ellen. <br /> Full Stack Developer <br /> UI/UX Researcher</h1>
+            <Typing>
+              <h1 className="Banner-title">Hi, I am Ellen. <br /> Full Stack Developer <br /> UI/UX enthusiast</h1>
+            </Typing>
             <img src={require('./assets/me2.JPG')} className="Banner-image" />
         </div>
       );
